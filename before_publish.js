53e7f3c3986323ae3d5e1a804e539a042735f5f3
uXiew <ChandlerVer5@gmail.com>
2026-04-02 19:21:20 +0800

    fix: track before_publish script

---
+++ before_publish.js
@@ -0,0 +1,28 @@
+import { dirname, join } from "path";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import pc from "picocolors";
+
+import replace from "replace-in";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+
+const [res] = await replace({
+  path: "./lib/constant.js",
+  request: [
+    {
+      regex: /__VERSION__/g,
+      replace: require(join(__dirname, "package.json")).version,
+    },
+  ],
+});
+
+if (res.isChanged) {
+  console.log(
+    pc.green("\n+ Version updated, replaced __VERSION__ in lib/constant.js"),
+  );
+} else {
+  throw new Error("Error: Version not updated!");
+}
